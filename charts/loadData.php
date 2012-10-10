d6b7876930476ffb1070ce8c4194ef781cab09dc
saschalion <saschalion@gmail.com>
2012-10-10 19:49:10 +0700

    Chart has been made

---
+++ charts/loadData.php
@@ -0,0 +1,9 @@
+<?php
+$arr = array(
+    array('data' => 1, 'tooltip' => 'name'),
+    array('data' => 10, 'tooltip' => 'name2'),
+    array('data' => 50, 'tooltip' => 'name3'),
+    array('data' => 40, 'tooltip' => 'name4')
+);
+
+echo json_encode($arr);
