2d60250c14ae6fd4092b3344de1169a049cb7153
saschalion <saschalion@gmail.com>
2012-10-11 17:57:28 +0700

    Stock chart has been made

--- charts/chart.php
+++ charts/chart.php
@@ -5,12 +5,6 @@
     <title>Параметры пульса</title>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
-    <?php if($_GET['stock']) { ?>
-        <script src="js/highstock.js"></script>
-        <script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
-        <script src="js/stock.js"></script>
-    <?php } ?>
-
     <?php if($_GET['pulse']) { ?>
         <script src="js/highcharts.js"></script>
         <script src="js/highcharts-more.js"></script>
@@ -18,13 +12,19 @@
         <script src="js/reports.js"></script>
         <script src="js/speed.js"></script>
     <?php } ?>
+
+    <?php if($_GET['pulse-advanced']) { ?>
+        <script src="js/highstock.js"></script>
+        <script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
+        <script src="js/stock.js"></script>
+    <?php } ?>
 </head>
 <body>
     <?php if($_GET['pulse']) {
         echo '<div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
         <div id="container-2" style="min-width: 400px; height: 400px; margin: 0 auto"></div>';
     }
-    elseif($_GET['stock']) {
+    elseif($_GET['pulse-advanced']) {
         echo '<div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>';
     } ?>
 </body>
