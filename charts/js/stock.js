353af944de44a238341debe60a10698a48d52f71
saschalion <saschalion@gmail.com>
2012-10-11 15:34:15 +0700

    Stock chart has been made

---
+++ charts/js/stock.js
@@ -0,0 +1,143 @@
+$(function () {
+    $(document).ready(function() {
+
+        var chart;
+
+        var options = {
+            chart: {
+                renderTo: 'container'
+            },
+            title: {
+                text: 'Параметры пульса'
+            },
+            subtitle: {
+                text: 'с октября 1999 до ноября 2012'
+            },
+            rangeSelector : {
+                selected : 1
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Пульс'
+                },
+                minorGridLineWidth: 0,
+                gridLineWidth: 1,
+                alternateGridColor: null,
+                max: 120,
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }],
+                labels: {
+                    formatter: function() {
+                        return this.value +' уд/мин';
+                    }
+                },
+                plotBands: [{
+                    from: 56,
+                    to: 66,
+                    color: 'rgba(0, 255, 0, 0.1)',
+                    label: {
+                        text: 'Норма',
+                        style: {
+                            color: '#000'
+                        }
+                    }
+                }, {
+                    from: 30,
+                    to: 56,
+                    color: 'rgba(255, 0, 0, 0.1)',
+                    label: {
+                        text: 'Ниже нормы',
+                        style: {
+                            color: '#000'
+                        }
+                    }
+                }, {
+                    from: 66,
+                    to: 120,
+                    dashStyle : 'shortdash',
+                    color: 'rgba(255, 0, 0, 0.1)',
+                    label: {
+                        text: 'Выше нормы',
+                        style: {
+                            color: '#000'
+                        }
+                    }
+                }]
+            },
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        style: {
+                            font: 'bold 9px Arial',
+                            verticalAlign: 'top'
+                        },
+                        enabled: true,
+                        formatter: function() {
+                            return + this.y + ' (' + this.point.config.tooltip + ')';
+                        }
+                    }
+                },
+                spline: {
+                    lineWidth: 4,
+                    states: {
+                        hover: {
+                            lineWidth: 5
+                        }
+                    },
+                    marker: {
+                        enabled: true,
+                        radius: 5,
+                        states: {
+                            hover: {
+                                enabled: true,
+                                symbol: 'circle',
+                                radius: 7,
+                                lineWidth: 1
+                            }
+                        }
+                    }
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            series: [{
+                marker : {
+                    enabled : true,
+                    radius : 3
+                },
+                name: 'Пульс',
+                data: [],
+                cursor: 'pointer',
+                tooltip: {
+                    formatter: function() {
+                        return + this.y + ' (' + this.point.config.tooltip + ')';
+                    },
+                    valueDecimals: 0
+                }
+            }]
+            ,
+            navigation: {
+                menuItemStyle: {
+                    fontSize: '10px'
+                }
+            }
+        };
+
+        $.getJSON('loadData.php?stock=true', function(data) {
+            yData = options.series[0].data;
+
+            for (i = 0; i < data.length; i++) {
+                yData.push(data[i]);
+            }
+
+            var chart = new Highcharts.StockChart(options);
+        });
+    });
+});
