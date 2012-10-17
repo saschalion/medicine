c4baa5ba75a95c6c4fc123e88c85f048d1637287
saschalion <saschalion@gmail.com>
2012-10-17 14:52:57 +0700

    Includes have been added

---
+++ demo/includes/call.php
@@ -0,0 +1,43 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#">МедОКБ</a>
+            <div class="nav-collapse collapse" style="height: 0px; ">
+                <ul class="nav">
+                    <li class="active">
+                        <a href="#">Мои пациенты</a>
+                    </li>
+                    <li><a href="#">Документы</a></li>
+                    <li><a href="#">Услуги</a></li>
+                    <li><a href="#">Записи</a></li>
+                </ul>
+                <div class="btn-group pull-right gap-left-out_10">
+                    <a class="btn" href="#">Профиль</a>
+                    <a class="btn dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#">
+                                Мой график
+                            </a>
+                        </li>
+                        <li>
+                            <a href="?logout=true">
+                                Выход
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <p class="navbar-text pull-right">
+                    Вы зашли как <span class="auth-text">гость</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
